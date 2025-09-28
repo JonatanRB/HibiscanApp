53143cf7b4358e6ea9565c015a6ae59414399eda
Jonatan <jonreabarbosa@gmail.com>
2025-09-28 14:03:17 -0600

    Feay: Cambio de valores 20, 15, 2, 0.3

--- hibiscus_ga_counter.py
+++ hibiscus_ga_counter.py
@@ -155,7 +155,7 @@ def count_objects(mask: np.ndarray, img: np.ndarray, min_area: int, max_area: in
 # -----------------------------
 
 class SimpleGA:
-    def __init__(self, pop_size=30, generations=25, elite=4, mutation_prob=0.35):
+    def __init__(self, pop_size=20, generations=15, elite=2, mutation_prob=0.3):
         self.pop_size = pop_size #Tamaño de la poblacion
         self.generations = generations #Numero de generaciones
         self.elite = elite #Mejores individuos que se conservan
